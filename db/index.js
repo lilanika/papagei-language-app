5666194eeb2da2fedfef46a78ef003263674b7fe
unknown <nikonova@hotmail.de>
2021-12-13 23:21:50 +0100

    backend working again

--- db/index.js
+++ db/index.js
@@ -1,12 +1,13 @@
 // ℹ️ package responsible to make the connection with mongodb
 // https://www.npmjs.com/package/mongoose
 const mongoose = require("mongoose");
+
 require("dotenv/config");
 // ℹ️ Sets the MongoDB URI for our app to have access to it.
 // If no env has been set, we dynamically set it to whatever the folder name was upon the creation of the app
 
 //const MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/tandemapp";
-const MONGODB_URI = process.env.MONGODB_URI || "mongodb+srv://lila:Uy0YXutdQ7O3CvUr@cluster0.lj0j1.mongodb.net/PapageiDataBase?retryWrites=true&w=majority";
+const MONGODB_URI = process.env.MONGODB_URI 
 mongoose
   .connect(MONGODB_URI, {
     useNewUrlParser: true,

3c128042798f1f9499bb9619710e1de01aae6506
lila <vivalafabulous@gmail.com>
2023-05-09 21:44:15 +0200

    update MongoDb

--- client/src/components/AboutPapagei.js
+++ client/src/components/AboutPapagei.js
@@ -6,7 +6,10 @@ export default class AboutPapagei extends Component {
   render() {
     return (
       <section class="m-top-landing max-width-landing ">
-        <section class="grid-container max-width-landing ">
+        <section
+          data-cy="AboutPapagei"
+          class="grid-container max-width-landing "
+        >
           <div class="reg ">
             <h1> Start talking to native speakers</h1>
             <Link to="/signup" className="   btn btn-light my-3 flex-smart">

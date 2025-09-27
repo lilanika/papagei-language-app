a1f81927710cdc87860e0ef3068d5243ffde2b72
lila <vivalafabulous@gmail.com>
2025-09-27 18:10:32 +0200

    fixed:sending two responses for the same request

--- routes/file-upload.js
+++ routes/file-upload.js
@@ -1,38 +1,41 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 
 // include CLOUDINARY:
 
-const uploader = require('../config/cloudinary-setup'); 
-const User = require('../models/User.model');
+const uploader = require("../config/cloudinary-setup");
+const User = require("../models/User.model");
 
+router.post(
+  "/upload/:userId",
+  uploader.single("imageUrl"),
+  (req, res, next) => {
+    console.log(" Ola from req.params", req.params);
+    console.log(" Ola from req.file.path ", req.file.path);
 
-router.post('/upload/:userId', uploader.single('imageUrl'), (req, res, next) => {
-  
-  console.log(' Ola from req.params', req.params)
-  console.log(' Ola from req.file.path ', req.file.path )
+    if (!req.file) {
+      next(new Error("No file uploaded!"));
+      return;
+    }
 
-  if (!req.file) {
-    next(new Error('No file uploaded!'));
-    return;
+    User.findByIdAndUpdate(
+      req.params.userId,
+      {
+        imageUrl: req.file.path,
+      },
+      { new: true }
+    )
+      .then((user) => {
+        console.log(user);
+        return res.json(user); // ← ADD 'return' HERE
+      })
+      .catch((error) => {
+        console.log(error);
+        return res.status(500).json({ error: "Update failed" }); // ← ADD ERROR HANDLING
+      });
+
+    // REMOVE THIS LINE: res.json({ secure_url: req.file.path });
   }
-
-  User.findByIdAndUpdate(req.params.userId, {
-    imageUrl: req.file.path
-
-  } , {new: true}).then(user => {
-    console.log(user);
-    res.json(user)
-   
-   });
-   
-
-
-
-  // get secure_url from the file object and save it in the
-  // variable 'secure_url', but this can be any name, just make sure you remember to use the same in frontend
-
-  res.json({ secure_url: req.file.path });
-});
+);
 
 module.exports = router;
